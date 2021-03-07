317dc1eda70969af4f7e216825f85023f1edeb02
Sebastian F <sebpfave2@gmail.com>
2021-03-06 22:36:53 -0500

    Create init func gets game stat data and displays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,34 @@
 // DOM selectors
 btnStart = document.getElementById("btn-start");
 timer = document.getElementById("round-timer");
+numWins = document.getElementById("num-wins");
+numLosses = document.getElementById("num-losses");
 
 // Variables
 const roundTime = 3; // seconds
+let gameStats = {};
 
 // Functions
+function init() {
+  getGameStats();
+  renderGameStats();
+}
+
+function getGameStats() {
+  const savedGameStats = JSON.parse(localStorage.getItem("gameStats"));
+
+  if (savedGameStats !== null) {
+    gameStats = savedGameStats;
+  } else {
+    gameStats = { wins: 0, losses: 0 };
+  }
+}
+
+function renderGameStats() {
+  numWins.textContent = gameStats.wins;
+  numLosses.textContent = gameStats.losses;
+}
+
 function startRound() {
   startTimer(roundTime);
 }
@@ -30,3 +53,6 @@ function startTimer(countDownTime) {
 btnStart.addEventListener("click", () => {
   startRound();
 });
+
+// Website Execution
+init();
